0407a8429148a200507462dbbeeb031d19b0768b
vigneshoffl <toconnectvignesh@gmail.com>
2023-11-14 23:57:32 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let selectFiled = document.getElementById("select-filed");
+let selectText = document.getElementById("select-text");
+let options = document.getElementsByClassName("options");
+let list = document.getElementById("list");
+let arrowIcon = document.getElementById("arrow-icon");
+
+selectFiled.onclick = function(){
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+}
+
+for(option of options){
+    option.onclick = function (){
+        selectText.innerHTML = this.textContent;
+        list.classList.toggle("hide");
+        arrowIcon.classList.toggle("rotate");
+    }
+}
